db14d1cc9d12952b8e00c6d1bca0355161879dcc
Maciej Hełmecki <maciej.helmecki@betssongroup.com>
2023-06-14 12:25:55 +0200

    Implement basic app behavior

---
+++ Storm Viewer/Storm Viewer/DetailViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Maciej Helmecki on 14/06/2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    // MARK: - Properties
+    
+    var selectedImageName: String?
+    
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    // MARK: - Methods
+    
+    private func setupView() {
+        title = selectedImageName
+        navigationItem.largeTitleDisplayMode = .never
+        view.backgroundColor = .white
+        
+        selectedImageName.map { imageView.image = UIImage(named: $0) }
+
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
--- Storm Viewer/Storm Viewer/ViewController.swift
+++ Storm Viewer/Storm Viewer/ViewController.swift
@@ -7,11 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    // MARK: - Properties
+    
+    private var imageNames = [String]()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        setupView()
+        fetchItems()
+    }
+    
+    // MARK: - Methods
+    
+    private func setupView() {
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func fetchItems() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                imageNames.append(item)
+            }
+        }
+    }
+}
+
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageNames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = imageNames[indexPath.row]
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.selectedImageName = imageNames[indexPath.row]
+        
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
