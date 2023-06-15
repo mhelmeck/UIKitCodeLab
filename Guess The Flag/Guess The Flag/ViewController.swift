83da02477083ca4c26a0d1e8c6fc65089bfba823
Maciej Hełmecki <maciej.helmecki@betssongroup.com>
2023-06-16 01:58:17 +0200

    Create empty Guess The Flag project

---
+++ Guess The Flag/Guess The Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess The Flag
+//
+//  Created by Maciej Helmecki on 16/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
