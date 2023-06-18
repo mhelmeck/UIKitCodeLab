aa0bd97ca30c6ca255a4b26d23a3c664cf24187a
Maciej Hełmecki <maciej.helmecki@betssongroup.com>
2023-06-18 17:49:52 +0200

    Add gaming logic

--- Guess The Flag/Guess The Flag/ViewController.swift
+++ Guess The Flag/Guess The Flag/ViewController.swift
@@ -11,10 +11,19 @@ import SnapKit
 class ViewController: UIViewController {
     // MARK: - Properties
     
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 20.0
+        
+        return view
+    }()
+    
     private let flagOne: UIButton = {
         let button = UIButton(type: .system)
         button.layer.borderWidth = 1
         button.layer.borderColor = UIColor.lightGray.cgColor
+        button.tag = 0
         
         return button
     }()
@@ -23,20 +32,23 @@ class ViewController: UIViewController {
         let button = UIButton(type: .system)
         button.layer.borderWidth = 1
         button.layer.borderColor = UIColor.lightGray.cgColor
+        button.tag = 1
         
         return button
     }()
     
     private let flagThree: UIButton = {
         let button = UIButton(type: .system)
-        button.layer.borderWidth = 1    
+        button.layer.borderWidth = 1
         button.layer.borderColor = UIColor.lightGray.cgColor
+        button.tag = 2
         
         return button
     }()
     
     private var countries = [String]()
     private var score = 0
+    private var correctAnswer = 0
     
     // MARK: - Lifecycle
     
@@ -52,25 +64,40 @@ class ViewController: UIViewController {
     private func setupView() {
         view.backgroundColor = .white
         
-        [flagOne, flagTwo, flagThree].forEach(view.addSubview)
+        flagOne.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        flagTwo.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        flagThree.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        view.addSubview(stackView)
+        [flagOne, flagTwo, flagThree].forEach(stackView.addArrangedSubview)
         
         installConstraints()
     }
     
+    @objc private func buttonTapped(sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let alertVC = UIAlertController(
+            title: title,
+            message: "Your score is \(score).",
+            preferredStyle: .alert
+        )
+        alertVC.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(alertVC, animated: true)
+    }
+    
     private func installConstraints() {
-        flagOne.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(100.0)
-            $0.centerX.equalToSuperview()
-        }
-        
-        flagTwo.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(230.0)
-            $0.centerX.equalToSuperview()
-        }
-        
-        flagThree.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(360.0)
-            $0.centerX.equalToSuperview()
+        stackView.snp.makeConstraints {
+            $0.centerY.centerX.equalToSuperview()
         }
     }
     
@@ -91,9 +118,14 @@ class ViewController: UIViewController {
         ].forEach( { countries.append($0) })
     }
     
-    private func askQuestion() {
+    private func askQuestion(action: UIAlertAction? = nil) {
+        countries.shuffle()
+        
         flagOne.setBackgroundImage(UIImage(named: countries[0]), for: .normal)
         flagTwo.setBackgroundImage(UIImage(named: countries[1]), for: .normal)
         flagThree.setBackgroundImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
     }
 }
