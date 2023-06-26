96397a383b62897d2ec161aa7c7c3a77fe87e5eb
Maciej Hełmecki <maciej.helmecki@betssongroup.com>
2023-06-26 14:35:59 +0200

    Create empty Easy Browser project

---
+++ Easy Browser/Easy Browser/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Maciej Helmecki on 26/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()     
+    }
+}
