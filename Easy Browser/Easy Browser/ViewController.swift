cb95d189d681a823c581ae05e0dfef62ab0aff22
Maciej Hełmecki <maciej.helmecki@betssongroup.com>
2023-06-28 17:07:10 +0200

    Add some functionality to the web browser app

--- Easy Browser/Easy Browser/ViewController.swift
+++ Easy Browser/Easy Browser/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
     // MARK: - Properties
     
     private var webView: WKWebView!
+    private var progressView: UIProgressView!
+    private let websites = ["google.com", "github.com"]
     
     // MARK: - Lifecycle
     
@@ -26,15 +28,37 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
-
-        load(stringUrl: "https://www.google.com")
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progresButton = UIBarButtonItem(customView: progressView)
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        let back = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+        let forward = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
+        
+        toolbarItems = [refresh, progresButton, spacer, back, forward]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        load(stringUrl: "https://\(websites[0])")
     }
     
     // MARK: - Methods
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
     @objc private func openTapped() {
         let alert = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        alert.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        for website in websites {
+            alert.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
         alert.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
@@ -57,5 +81,24 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        let alertVC = UIAlertController(title: "Sorry", message: "This side is blocked", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(alertVC, animated: true)
+        
+        decisionHandler(.cancel)
     }
 }
