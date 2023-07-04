75e67efb70509d7d6df40d0efc2f983809c8da7b
Maciej Hełmecki <maciej.helmecki@betssongroup.com>
2023-07-04 15:01:39 +0200

    Start implementing view models

--- Storm Viewer/Storm Viewer/Sources/HomeTableViewController.swift
+++ Storm Viewer/Storm Viewer/Sources/HomeTableViewController.swift
@@ -5,12 +5,47 @@
 //  Created by Maciej Helmecki on 14/06/2023.
 //
 
+import Combine
 import UIKit
+
+class HomeTableViewModel {
+    // MARK: - Properties
+    
+    @Published private(set) var models = [CellModel]()
+    
+    private(set) var selectedImageTitle = String()
+    var numberOfRows: Int { models.count }
+    
+    // MARK: - Init
+    
+    init() {
+        fetchItems()
+    }
+    
+    // MARK: - Methods
+    
+    private func fetchItems() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                models.append(CellModel(title: item))
+            }
+        }
+        
+        models.sort()
+    }
+}
+
+// MARK: - HomeTableViewController
 
 class HomeTableViewController: UITableViewController {
     // MARK: - Properties
     
-    private var imageNames = [String]()
+    private var cancellables = Set<AnyCancellable>()
+    private var viewModel: HomeTableViewModel!
     
     // MARK: - Lifecycle
     
@@ -20,8 +55,12 @@ class HomeTableViewController: UITableViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(shareTapped))
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
+        viewModel = HomeTableViewModel()
+        
         setupView()
-        fetchItems()
+        bindView()
+
+        
     }
     
     // MARK: - Methods
@@ -31,39 +70,33 @@ class HomeTableViewController: UITableViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
-    private func fetchItems() {
-        let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
-
-        for item in items {
-            if item.hasPrefix("nssl") {
-                imageNames.append(item)
-            }
-        }
-        
-        imageNames.sort()
+    private func bindView() {
+        viewModel.$models
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.tableView.reloadData()
+            }.store(in: &cancellables)
     }
 }
 
 extension HomeTableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imageNames.count
+        return viewModel.numberOfRows
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
-        cell.textLabel?.text = imageNames[indexPath.row]
+        cell.textLabel?.text = viewModel.models[indexPath.row].title
         
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.selectedImageName = imageNames[indexPath.row]
+        vc.selectedImageName = viewModel.models[indexPath.row].title
         
-        let imagesAmount = imageNames.count
+        let imagesAmount = viewModel.models.count
         let selectedImagePosition = indexPath.row + 1
         let screenTitle = "Picture \(selectedImagePosition) of \(imagesAmount)"
         vc.screenTitle = screenTitle
@@ -80,5 +113,15 @@ extension HomeTableViewController {
             vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
             present(vc, animated: true)
         }
+    }
+}
+
+struct CellModel {
+    let title: String
+}
+
+extension CellModel: Comparable {
+    static func < (lhs: CellModel, rhs: CellModel) -> Bool {
+        lhs.title < rhs.title
     }
 }
