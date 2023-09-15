b2e2a4540c885a114245bbf1f47adc19fd10efa1
Maciej Hełmecki <maciej.helmecki@betssongroup.com>
2023-09-15 21:42:17 +0200

    Add empty ipad Seven Little Words project

---
+++ Seven Little Words/Seven Little Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seven Little Words
+//
+//  Created by Maciej Helmecki on 15/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
