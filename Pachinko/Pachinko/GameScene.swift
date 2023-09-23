14f71718338c69311272d85c6443791c017f386b
Maciej Hełmecki <maciej.helmecki@betssongroup.com>
2023-09-24 01:27:40 +0200

    Add some more phisics

--- Pachinko/Pachinko/GameScene.swift
+++ Pachinko/Pachinko/GameScene.swift
@@ -23,8 +23,11 @@ class GameScene: SKScene {
             let location = touch.location(in: self)
             let ball = SKSpriteNode(imageNamed: "ballRed")
             ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-            ball.physicsBody?.restitution = 0.4
+            ball.physicsBody!.restitution = 0.4
+            ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
             ball.position = location
+            ball.name = "ball"
+            
             
             addChild(ball)
         }
@@ -35,24 +38,81 @@ class GameScene: SKScene {
     private func setupView() {
         addChild(backgroundNode)
         
-        [
-            (x: 0, y: 0),
-            (x: 256, y: 0),
-            (x: 512, y: 0),
-            (x: 768, y: 0),
-            (x: 1024, y: 0)
-        ].map(CGPoint.init).map(makeBouncer).forEach(addChild)
+        let bouncerCoords = [ (x: 0, y: 0), (x: 256, y: 0), (x: 512, y: 0), (x: 768, y: 0), (x: 1024, y: 0)]
+        let slotCoords = [ (x: 128, y: 0), (x: 384, y: 0), (x: 640, y: 0), (x: 896, y: 0)]
+            
+        bouncerCoords
+            .map(CGPoint.init)
+            .forEach(addBouncer)
+        slotCoords
+            .map(CGPoint.init)
+            .enumerated()
+            .forEach(addSlot)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.contactDelegate = self
     }
     
-    private func makeBouncer(at position: CGPoint) -> SKSpriteNode {
+    private func addBouncer(at position: CGPoint) {
         let node = SKSpriteNode(imageNamed: "bouncer")
         node.position = position
         node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2.0)
         node.physicsBody?.isDynamic = false
         
-        return node
+        addChild(node)
+    }
+    
+    private func addSlot(details: (offset: Int, element: CGPoint)) {
+        let position = details.element
+        var slotBase: SKSpriteNode
+        var slotGlow: SKSpriteNode
+
+        if details.offset % 2 == 0 {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
+        } else {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
+        }
+        
+        slotBase.position = position
+        slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
+        slotBase.physicsBody?.isDynamic = false
+        
+        let spin = SKAction.rotate(byAngle: .pi, duration: 10)
+        let spinForever = SKAction.repeatForever(spin)
+        slotGlow.position = position
+        slotGlow.run(spinForever)
+        
+        addChild(slotBase)
+        addChild(slotGlow)
+    }
+    
+    private func collisionBetween(ball: SKNode, object: SKNode) {
+        if object.name == "good" {
+            destroy(ball: ball)
+        } else if object.name == "bad" {
+            destroy(ball: ball)
+        }
+    }
+
+    private func destroy(ball: SKNode) {
+        ball.removeFromParent()
+    }
+}
+
+extension GameScene: SKPhysicsContactDelegate {
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+
+        if nodeA.name == "ball" {
+            collisionBetween(ball: nodeA, object: nodeB)
+        } else if nodeB.name == "ball" {
+            collisionBetween(ball: nodeB, object: nodeA)
+        }
     }
 }
 
