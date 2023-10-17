80c9104f6f0e8a30545ced976f877a5d84395dcd
Maciej Hełmecki <maciej.helmecki@betssongroup.com>
2023-10-17 15:02:36 +0200

    Create empty insta filter project

---
+++ Instafilter/Instafilter/Source/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Maciej Helmecki on 17/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
